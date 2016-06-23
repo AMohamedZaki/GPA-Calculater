2e5ecbf32482da8e875244ba5a86c082b3331e38
amohamed <amohamed@acs-me.biz>
2016-06-23 16:08:14 +0200

    The DB Script

---
+++ DbSecript.sql
@@ -0,0 +1,16 @@
+﻿--
+CREATE TABLE [dbo].[Gpas] (
+    [Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [Name]     NVARCHAR (MAX) NULL,
+    [DateTime] DATETIME       NOT NULL,
+    CONSTRAINT [PK_dbo.Gpas] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+--
+CREATE TABLE [dbo].[Grades] (
+    [Id]    INT             IDENTITY (1, 1) NOT NULL,
+    [Name]  NVARCHAR (MAX)  NULL,
+    [Value] DECIMAL (18, 2) NOT NULL,
+    [GpaId] INT             NOT NULL,
+    CONSTRAINT [PK_dbo.Grades] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_dbo.Grades_dbo.Gpas_GpaId] FOREIGN KEY ([GpaId]) REFERENCES [dbo].[Gpas] ([Id]) 
+);
